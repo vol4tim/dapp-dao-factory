9090cd3ae1b73bfc1dc4f0dd5d8334194e658607
vol4tim <vol4tim@gmail.com>
2016-08-10 15:29:48 +0400

    monitoring latest block blockchain

--- src/utils/dao_factory.js
+++ src/utils/dao_factory.js
@@ -54,15 +54,27 @@ export function removeModule(dao, name) {
                 gas:  3000000
             }
         ];
-        dao.removeModule.apply(dao, args);
-		resolve();
+        var tx = dao.removeModule.apply(dao, args);
+		blockchain.subscribeTx(tx).
+			then(()=>{
+				resolve();
+			}).
+			catch(function(e) {
+				console.log(e);
+			});
     });
 }
 
 export function linkCore(core, module, address) {
     return new Promise(function(resolve) {
-        core.setModule(module, address, 'abi', true, {from:web3.eth.accounts[0], gas:300000})
-        resolve(address);
+        var tx = core.setModule(module, address, 'abi', true, {from:web3.eth.accounts[0], gas:300000})
+        blockchain.subscribeTx(tx).
+			then(()=>{
+				resolve(address);
+			}).
+			catch(function(e) {
+				console.log(e);
+			});
     });
 }
 
@@ -84,3 +96,77 @@ export function loadAbis(abis) {
 export function getContract(abi, address) {
     return web3.eth.contract(abi).at(address)
 }
+
+export class Blockchain {
+	subscribes = []
+	web3 = false
+    constructor(web3) {
+		this.web3 = web3
+		if (this.web3) {
+			this.observeLatestBlocks()
+		}
+    }
+	observeLatestBlocks() {
+		var self = this
+		this.web3.eth.filter('latest').watch(function(e, hash){
+			if (!e) {
+				var info = self.web3.eth.getBlock(hash);
+				_.forEach(self.subscribes, function(item) {
+					if (_.isFunction(item)) {
+						item(hash)
+					} else {
+						if (_.findIndex(info.transactions, (i)=>i==item.tx) >= 0) {
+							var transaction = self.web3.eth.getTransaction(item.tx)
+							if (transaction) {
+								item.cb(transaction)
+								_.remove(self.subscribes, function(f) {
+									return !_.isFunction(f) && f.tx == item.tx;
+								});
+							}
+						}
+					}
+				})
+			}
+		});
+	}
+    setSubscribe(cb) {
+        this.subscribes.push(cb)
+    }
+	observeBlock() {
+		var self = this
+		return new Promise(function(resolve) {
+			self.setSubscribe(function() {
+				resolve()
+			});
+		});
+	}
+	subscribeTx(tx) {
+		var self = this
+		return new Promise(function(resolve) {
+			self.setSubscribe({
+				tx: tx,
+				cb: function(tx) {
+					resolve(tx)
+				}
+			});
+		});
+	}
+}
+
+export const blockchain = new Blockchain(getWeb3())
+
+// blockchain.subscribeTx('0x111111').
+// 	then((tx)=>{
+// 		console.log('tx', tx);
+// 	}).
+// 	catch(function(e) {
+// 		console.log(e);
+// 	});
+//
+// blockchain.setSubscribe(()=>{
+// 	console.log('update 1');
+// })
+//
+// blockchain.setSubscribe(()=>{
+// 	console.log('update 2');
+// })
